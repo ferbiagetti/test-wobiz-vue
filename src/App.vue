8002e513918d8b9f11bcd655132893c0dbc426e2
Fernando <biagettifernando@gmail.com>
2020-10-04 22:43:14 -0300

    Se suben los archivos faltantes

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
+<style lang="scss">
+  @import './App.scss';
+</style>
+
 <template>
   <div id="app">
     <router-view/>
   </div>
 </template>
-
-<style lang="scss">
-  @import './App.scss';
-
-
-</style>
